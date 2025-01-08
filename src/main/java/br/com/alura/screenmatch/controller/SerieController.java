7014d6254d961edcad4322e149b83628dac62184
domingos <mds.junior87@gmail.com>
2025-01-07 23:59:35 -0300

    ✨ Implementação do front-end da aplicação ScreenMatch com suporte para visualização via Live Server

---
+++ src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -0,0 +1,32 @@
+package br.com.alura.screenmatch.controller;
+
+import br.com.alura.screenmatch.dto.SerieDTO;
+import br.com.alura.screenmatch.service.SerieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class SerieController {
+
+
+    @Autowired
+    private SerieService servico;
+
+    @GetMapping("/series")
+    public List<SerieDTO> obterSeries() {
+        return servico.obterTodasAsSeries();
+
+
+    }
+
+    @GetMapping("/series/top5")
+    public List<SerieDTO> obterTop5Series() {
+        return servico.obterTop5Series();
+    }
+
+
+
+}
---
+++ src/main/java/br/com/alura/screenmatch/service/SerieService.java
@@ -0,0 +1,34 @@
+package br.com.alura.screenmatch.service;
+
+import br.com.alura.screenmatch.dto.SerieDTO;
+import br.com.alura.screenmatch.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SerieService {
+
+    @Autowired
+    private SerieRepository repositorio;
+
+    public List<SerieDTO> obterTodasAsSeries() {
+        return repositorio.findAll()
+                .stream()
+                .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(),
+                        s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
+                .collect(java.util.stream.Collectors.toList());
+    }
+
+
+    public List<SerieDTO> obterTop5Series() {
+        return repositorio.findTop5ByOrderByAvaliacaoDesc()
+                .stream()
+                .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(),
+                        s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
+                .collect(java.util.stream.Collectors.toList());
+
+    }
+}
+
